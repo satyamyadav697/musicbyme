b1d8215da785218d07673d8abc28a5962bb14cd2
Team DeadlineTech <tosu80018@gmail.com>
2025-05-29 18:33:37 +0530

    Update __main__.py

--- DeadlineTech/__main__.py
+++ DeadlineTech/__main__.py
@@ -15,19 +15,19 @@ from DeadlineTech.utils.database import get_banned_users, get_gbanned
 from config import BANNED_USERS
 from DeadlineTech.plugins.misc.auto_leave import scheduled_auto_leave
 
+
+
 async def init():
-  if (
+    if (
         not config.STRING1
         and not config.STRING2
         and not config.STRING3
         and not config.STRING4
         and not config.STRING5
-     ):
+    ):
         LOGGER(__name__).error("Assistant client variables not defined, exiting...")
         exit()
-
     await sudo()
-
     try:
         users = await get_gbanned()
         for user_id in users:
@@ -37,37 +37,31 @@ async def init():
             BANNED_USERS.add(user_id)
     except:
         pass
-
     await app.start()
-
-
     asyncio.create_task(scheduled_auto_leave())
-
     for all_module in ALL_MODULES:
         importlib.import_module("DeadlineTech.plugins" + all_module)
     LOGGER("DeadlineTech.plugins").info("Successfully Imported Modules...")
-
     await userbot.start()
     await Anony.start()
-
     try:
         await Anony.stream_call("https://te.legra.ph/file/29f784eb49d230ab62e9e.mp4")
     except NoActiveGroupCall:
-    LOGGER("DeadlineTech").error(
-            "Please turn on the videochat of your log group/channel.\n\nStopping Bot..."
-        )
-        exit()
-    except:
-        pass
+        LOGGER("DeadlineTech").error(
+            "Please turn on the videochat of your log group\channel.\n\nStopping Bot..."
+        )
+        exit()
+    except:
+        pass
+    await Anony.decorators()
+    LOGGER("DeadlineTech").info(
+        "DeadlineTech Music Bot started successfully"
+    )
+    await idle()
+    await app.stop()
+    await userbot.stop()
+    LOGGER("DeadlineTech").info("Stopping DeadlineTech Music Bot...")
 
-    await Anony.decorators()
-    LOGGER("DeadlineTech").info("DeadlineTech Music Bot started successfully")
-
-    await idle()
-
-    await app.stop()
-    await userbot.stop()
-    LOGGER("DeadlineTech").info("Stopping DeadlineTech Music Bot...")
 
 if __name__ == "__main__":
-    asyncio.get_event_loop().run_until_complete(init())
+    asyncio.get_event_loop().run_until_complete(init())
