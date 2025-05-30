c5e35505e2fa4317c13ffdac25a422c3a28ef5e5
Team DeadlineTech <tosu80018@gmail.com>
2025-05-31 02:31:32 +0530

    Update broadcast.py

--- DeadlineTech/plugins/misc/broadcast.py
+++ DeadlineTech/plugins/misc/broadcast.py
@@ -7,6 +7,7 @@ import asyncio
 from pyrogram import filters
 from pyrogram.enums import ChatMembersFilter
 from pyrogram.errors import FloodWait, RPCError
+from pyrogram.types import Message
 
 from DeadlineTech import app
 from DeadlineTech.misc import SUDOERS
