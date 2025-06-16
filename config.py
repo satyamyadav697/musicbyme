2ceb0f8b0bd4f161395fae509cb1c0d621bf5170
Team DeadlineTech <tosu80018@gmail.com>
2025-06-17 03:18:40 +0530

    Update config.py

--- config.py
+++ config.py
@@ -22,9 +22,6 @@ MONGO_DB_URI = getenv("MONGO_DB_URI", None)
 
 DURATION_LIMIT_MIN = int(getenv("DURATION_LIMIT", 60))
 
-# Set this to true if you want post ads automatically
-ADS_MODE = getenv("ADS_MODE", None)
-
 # Chat id of a group for logging bot's activities
 LOGGER_ID = int(getenv("LOGGER_ID", None))
 
