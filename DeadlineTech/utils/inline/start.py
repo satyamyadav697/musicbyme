da168bdb96842eaa9c08ab31940c541999071277
Team DeadlineTech <tosu80018@gmail.com>
2025-06-17 03:22:31 +0530

    Update start.py

--- DeadlineTech/utils/inline/start.py
+++ DeadlineTech/utils/inline/start.py
@@ -30,6 +30,7 @@ def private_panel(_):
             )
         ],
         [
+            InlineKeyboardButton(text=_["S_B_10"], user_id=config.OWNER_ID), 
             InlineKeyboardButton(text=_["S_B_4"], callback_data="settings_back_helper")
         ],
 
